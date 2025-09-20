d14ef1170a9247293010eb63bb0a83dd413f9a47
itzzadityachaudhari <adit.chaudhari05@gmail.com>
2025-09-20 16:41:15 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
+    DB_HOST : 'database-1.cluster-czgkcmm6qc39.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Adit800020',
     DB_DATABASE : ''
 });
